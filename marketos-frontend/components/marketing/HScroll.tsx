c1a75cbcc9880ad3527f1bc2676b1f48bbe74061
Samriddha Roy <samriddharoy0804@gmail.com>
2026-07-10 16:42:49 +0530

    Delete marketos-frontend directory

--- marketos-frontend/components/marketing/HScroll.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface HScrollProps {
-  children: React.ReactNode;
-}
-
-export function HScroll({ children }: HScrollProps) {
-  return <div className="flex gap-4 overflow-x-auto pb-4">{children}</div>;
-}
